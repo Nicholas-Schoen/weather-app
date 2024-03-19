2a3303441454854eb16320711b69506d29b6f528
Nicholas Schoen <scheontransit@gmail.com>
2024-03-18 20:36:18 -0500

    added document variables

---
+++ index.js
@@ -0,0 +1,8 @@
+const location = document.getElementById("location-input");
+const button = document.getElementById("button");
+const cityName = document.getElementById("title-text");
+const tempature = document.getElementById("tempature");
+const display = document.getElementById("picture-display");
+const weatherDescription = document.getElementById("weather-description");
+const toggle = document.getElementById("toggle-switch");
+
