cd8f236bcf9fc371556375d97fa936408db8f39c
Nicholas Schoen <scheontransit@gmail.com>
2024-03-20 12:54:34 -0500

    finally figured out out how to log the lat and lon

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const tempature = document.getElementById("tempature");
 const display = document.getElementById("picture-display");
 const weatherDescription = document.getElementById("weather-description");
 const toggle = document.getElementById("toggle-switch");
+let latitude;
+let longitude;
 
 const geocodeApiKey = "65fa47d3931a3517084361bune5a288";
 
 const openWeatherApiKey = "01d54155ab7c730a05cdbd85326bd430";
 /*
-const openWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`;
+const openWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherApiKey}`;
 */
 function getGeocodeData() {
     const geocodeUrl = `https://geocode.maps.co/search?q=${locationInput.value}&api_key=${geocodeApiKey}`;
 
     fetch(geocodeUrl) 
-    .then(Response => {
-        if (!Response.ok) {
+    .then(response => {
+        if (!response.ok) {
             throw new Error("Response not ok")
         }
-        return Response.json();
+        return response.json();
     })
     .then(data => {
-        console.log("Raw JSON:", data);
+        console.log("Latitude: ", data[0].lat);
+        console.log("Longitude: ", data[0].lon);
+        latitude = data[0].lat;
+        longitude = data[0].lon;
     })
     .catch(error => {
         console.error("Error", error);
