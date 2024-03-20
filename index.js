b2abb0f4a86eacaf4d959b8f57d4c737d96c4a5c
Nicholas Schoen <scheontransit@gmail.com>
2024-03-20 13:39:39 -0500

    got the weather data to log out

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const tempature = document.getElementById("tempature");
 const display = document.getElementById("picture-display");
 const weatherDescription = document.getElementById("weather-description");
 const toggle = document.getElementById("toggle-switch");
+
+const geocodeApiKey = "65fa47d3931a3517084361bune5a288";
+const openWeatherApiKey = "01d54155ab7c730a05cdbd85326bd430";
+
 let latitude;
 let longitude;
 
-const geocodeApiKey = "65fa47d3931a3517084361bune5a288";
 
-const openWeatherApiKey = "01d54155ab7c730a05cdbd85326bd430";
-/*
-const openWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherApiKey}`;
-*/
 function getGeocodeData() {
     const geocodeUrl = `https://geocode.maps.co/search?q=${locationInput.value}&api_key=${geocodeApiKey}`;
 
@@ -29,12 +28,31 @@ function getGeocodeData() {
         console.log("Longitude: ", data[0].lon);
         latitude = data[0].lat;
         longitude = data[0].lon;
+        getLocationData();
     })
     .catch(error => {
         console.error("Error", error);
     })
 };
 
-function getLocationData() {};
+function getLocationData() {
+    const openWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${openWeatherApiKey}`;
+
+    fetch(openWeatherUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Response not ok")
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Raw weather API JSON data: ", data)
+    })
+};
+
+function getWeatherData() {
+    getGeocodeData;
+    getLocationData;
+}
 
 button.addEventListener("click", getGeocodeData);
