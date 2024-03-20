de82994f43e2b2e881fc90311bb2d69f144dab8f
Nicholas Schoen <scheontransit@gmail.com>
2024-03-20 04:14:30 -0500

    added in api keys and their appropriate url

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const location = document.getElementById("location-input");
+const locationInput = document.getElementById("location-input");
 const button = document.getElementById("button");
 const cityName = document.getElementById("title-text");
 const tempature = document.getElementById("tempature");
@@ -6,8 +6,15 @@ const display = document.getElementById("picture-display");
 const weatherDescription = document.getElementById("weather-description");
 const toggle = document.getElementById("toggle-switch");
 
-const geocodeApiKey = 65fa47d3931a3517084361bune5a288;
-const openWeatherApiKey = 01d54155ab7c730a05cdbd85326bd430;
+const geocodeApiKey = "65fa47d3931a3517084361bune5a288";
+const geocodeUrl = `https://geocode.maps.co/search?q=${locationInput.value}&api_key=${geocodeApiKey}`;
+
+const openWeatherApiKey = "01d54155ab7c730a05cdbd85326bd430";
+const openWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`;
+
+
+
+
 
 function getLocationData() {
 
