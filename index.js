1622870827088fc526f96cdabeb831267ce380ab
Nicholas Schoen <scheontransit@gmail.com>
2024-03-19 21:24:29 -0500

    add api keys needed for app

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const display = document.getElementById("picture-display");
 const weatherDescription = document.getElementById("weather-description");
 const toggle = document.getElementById("toggle-switch");
 
+const geocodeApiKey = 65fa47d3931a3517084361bune5a288;
+const openWeatherApiKey = 01d54155ab7c730a05cdbd85326bd430;
+
+function getLocationData() {
+
+};
+
+
+button.addEventListener("submit");
